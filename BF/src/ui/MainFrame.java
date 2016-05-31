752d6d1b6d6d380eeefa76aa70833f4b9d7ab829
lxs137 <lxs137@hotmail.com>
2016-05-31 09:55:26 +0800

    Login Dialog

--- BF/src/ui/MainFrame.java
+++ BF/src/ui/MainFrame.java
@@ -2,8 +2,10 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
@@ -11,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -19,12 +22,16 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import rmi.RemoteHelper;
 
 
 public class MainFrame extends JFrame 
 {
+	private JFrame mainFrame;
+	private JFrame loginFrame;
 	private JTextArea codeText;
 	private JTextArea paramText;
 	private JLabel resultLabel;
@@ -34,9 +41,9 @@ public class MainFrame extends JFrame
 	public MainFrame() 
 	{
 		// 创建窗体
-		JFrame frame = new JFrame("BF Client");
-		frame.setLayout(null);
-		frame.setResizable(false);
+		mainFrame= new JFrame("BF Client");
+		mainFrame.setLayout(null);
+		mainFrame.setResizable(false);
 
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
@@ -54,15 +61,27 @@ public class MainFrame extends JFrame
 		JMenu runMenu=new JMenu("Run");
 		runMenu.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
 		menuBar.add(runMenu);
+		JButton loginButton=new JButton("Login");
+		loginButton.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
+		menuBar.add(loginButton, -1);
 		JMenuItem runMenuItem = new JMenuItem("Run");
 		runMenuItem.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
 		runMenu.add(runMenuItem);
-		frame.setJMenuBar(menuBar);
+		mainFrame.setJMenuBar(menuBar);
 
 		newMenuItem.addActionListener(new MenuItemActionListener());
 		openMenuItem.addActionListener(new MenuItemActionListener());
 		saveMenuItem.addActionListener(new SaveActionListener());
 		runMenuItem.addActionListener(new MenuItemActionListener());
+		loginButton.addActionListener(new ActionListener() 
+		{			
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				initLoginDialog();
+				
+			}
+		});
 		
 		//显示代码
 		codeText = new JTextArea();
@@ -75,7 +94,7 @@ public class MainFrame extends JFrame
 		textScrollPane.setSize(600, 300);
 		textScrollPane.setLocation(0, 0);
 		textScrollPane.setBorder(BorderFactory.createEtchedBorder());
-		frame.add(textScrollPane);
+		mainFrame.add(textScrollPane);
 		
 		//显示提示
 		hintLabel=new JLabel();
@@ -84,7 +103,7 @@ public class MainFrame extends JFrame
 		hintLabel.setSize(580,30);
 		hintLabel.setLocation(8,310);
 		hintLabel.setBorder(BorderFactory.createBevelBorder(10));
-		frame.add(hintLabel);
+		mainFrame.add(hintLabel);
 
 		//显示输入
 		paramText=new JTextArea();
@@ -92,7 +111,7 @@ public class MainFrame extends JFrame
 		paramText.setFont(new Font(Font.MONOSPACED,Font.PLAIN,15));
 		paramText.setSize(290,50);
 		paramText.setLocation(8,350);
-		frame.add(paramText);
+		mainFrame.add(paramText);
 		
 		//显示结果
 		resultLabel = new JLabel();
@@ -100,12 +119,48 @@ public class MainFrame extends JFrame
 		resultLabel.setFont(new Font(Font.MONOSPACED,Font.PLAIN,15));	
 		resultLabel.setSize(290,50);
 		resultLabel.setLocation(300,350);
-		frame.add(resultLabel);
+		mainFrame.add(resultLabel);
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 500);
-		frame.setLocation(300, 100);
-		frame.setVisible(true);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(600, 500);
+		mainFrame.setLocation(300, 100);
+		mainFrame.setVisible(true);
+	}
+	
+	private void initLoginDialog()
+	{
+		loginFrame=new JFrame("Login");
+		loginFrame.setLayout(new GridLayout(3,1));
+		
+		JPanel usernameJPanel=new JPanel();
+		usernameJPanel.setLayout(new GridLayout(1,2));
+		JLabel usernameJLabel=new JLabel("Username");
+		JTextField usernameJText=new JTextField();
+		usernameJPanel.add(usernameJLabel);
+		usernameJLabel.add(usernameJText);
+		loginFrame.add(usernameJPanel);
+		
+		JPanel passwordJPanel=new JPanel();
+		passwordJPanel.setLayout(new GridLayout(1,2));
+		JLabel passwordJLabel=new JLabel("Password");
+		//passwordJLabel;
+		JTextField passwordJText=new JTextField();
+		passwordJPanel.add(passwordJLabel);
+		passwordJPanel.add(passwordJText);
+		loginFrame.add(passwordJPanel);
+		
+		JPanel buttonJPanel=new JPanel();
+		buttonJPanel.setLayout(new GridLayout(1,2));
+		JButton registerJButton=new JButton("Register");
+		JButton loginJButton=new JButton("Login");
+		buttonJPanel.add(loginJButton);
+		buttonJPanel.add(registerJButton);		
+		loginFrame.add(buttonJPanel);
+		
+		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		loginFrame.setSize((int)(mainFrame.getWidth()/1.5),mainFrame.getHeight()/2);
+		loginFrame.setLocation(mainFrame.getX()+100,mainFrame.getY()+100);
+		loginFrame.setVisible(true);
 	}
 
 	class MenuItemActionListener implements ActionListener 
@@ -156,5 +211,6 @@ public class MainFrame extends JFrame
 			}
 		}
 	}
+	
 	
 }
